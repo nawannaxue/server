fb98eecfc45da748233e8d49b7b48ecca61456c4
kaiqinsun <kaiqinsun@gmail.com>
2017-09-12 20:48:50 -0400

    Add Java 8 support for Jackson-jaxrs provider

--- server-web/src/main/java/com/nwnx/api/apps/RestApplication1.java
+++ server-web/src/main/java/com/nwnx/api/apps/RestApplication1.java
@@ -1,5 +1,10 @@
 package com.nwnx.api.apps;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +18,12 @@ public class RestApplication1 extends ResourceConfig {
     public RestApplication1() {
         logger.info("Starting registering REST api resources");
         packages(true, "com.nwnx.api");
+
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule());
+        JacksonJsonProvider jacksonJsonProvider = new JacksonJsonProvider(mapper);
+        registerInstances(jacksonJsonProvider);
     }
 }
