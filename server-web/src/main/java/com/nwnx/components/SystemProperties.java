dce02a01e56e515ccf6999dbd0a1da859bbbb478
kaiqinsun <kaiqinsun@gmail.com>
2017-09-24 17:27:52 -0400

    Check both env variable and system property to determine if it is a dev mode or production mode

--- server-web/src/main/java/com/nwnx/components/SystemProperties.java
+++ server-web/src/main/java/com/nwnx/components/SystemProperties.java
@@ -1,11 +1,20 @@
 package com.nwnx.components;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 public final class SystemProperties {
-    private static final boolean DEV_MODE = Optional.ofNullable(System.getenv("spring.profiles.active"))
-            .map(s -> s.equalsIgnoreCase("development"))
-            .orElse(false);
+    private static final String SPRING_PROFILES_ACTIVE = "spring.profiles.active";
+    private static final String DEVELOPMENT = "development";
+    private static final String PRODUCTION = "production";
+
+    private static final boolean DEV_MODE = Optional.ofNullable(System.getenv(SPRING_PROFILES_ACTIVE))
+            .map(DEVELOPMENT::equalsIgnoreCase)
+            .orElseGet(isDevProfileInSystemProperty());
+
+    private static Supplier<Boolean> isDevProfileInSystemProperty() {
+        return () -> System.getProperty(SPRING_PROFILES_ACTIVE, PRODUCTION).equalsIgnoreCase(DEVELOPMENT);
+    }
 
     private SystemProperties() {
     }
